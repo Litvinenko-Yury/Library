d5e2192350fc3a3ad3d7e9f33d2f541d1e01ae0b
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-03-17 11:34:58 +0300

    Updates

--- js/burgers-some.js
+++ js/burgers-some.js
@@ -9,9 +9,9 @@
     */
 /*=============================*/
 
-'use strict'
+'use strict';
 
-document.onclick = function (event) { /*1. вешаем единый обработчик на элемент document*/
+document.addEventListener('click', function (event) { /*1. вешаем единый обработчик на элемент document*/
   /*это нужно для того, что-бы много burger-ов можно было ставить в любом месте*/
   let target = event.target; /*2. где был клик?*/
   //console.log(target);
@@ -42,7 +42,44 @@ document.onclick = function (event) { /*1. вешаем единый обраб�
   };
 
   //console.log("вышел из цикла");
-};
+});
+
+
+
+/**====================================== */
+
+// document.onclick = function (event) { /*1. вешаем единый обработчик на элемент document*/
+//   /*это нужно для того, что-бы много burger-ов можно было ставить в любом месте*/
+//   let target = event.target; /*2. где был клик?*/
+//   //console.log(target);
+
+//   while (target != document) {
+//     if (target.classList.contains("burger" || "burger--close")) { /* 3. на .burger? то, что надо!*/
+//       //console.log("клик на бургер");
+//       target.classList.toggle("burger--close");
+
+//       let burgerDataID = target.getAttribute("data-target-id"); /*7. из переменной target читаем значение атрибута data-target-id*/
+//       //console.log("нашел data-target-id = " + burgerDataID);
+
+//       let burgerDataClassToggle = target.getAttribute("data-target-class-toggle"); /*8. из переменной target читаем значение атрибута data-target-class-toggle*/
+//       //console.log("нашел data-target-class-toggle = " + burgerDataClassToggle);
+
+//       let elementClassToggle = document.getElementById(burgerDataID); /* 9. находим элемент с нужным ID*/
+//       //console.log("нашел элемент с нужным ID = " + elementClassToggle);
+
+//       elementClassToggle.classList.toggle(burgerDataClassToggle);/* 10. Элементу с нужным ID меняем класс на переданный в data-target-class-toggle*/
+
+//       break;
+//     } else {/* 4. не на .burger? нет, не подходит... */
+//       //console.log("промазал");
+//       target = target.parentNode; /*5. поднимаем target на уровень вверх по иерархии родителей от event.target и выше*/
+//       //console.log("вывожу target на уровень выше");
+//       //console.log(target);
+//     }
+//   };
+
+//   //console.log("вышел из цикла");
+// };
 
 /* 6. Если клик был мимо burger, цикл дойдет вверх до document, и обработчик закончит работу*/
 
