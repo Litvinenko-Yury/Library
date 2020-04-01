f65d6c792db2c5e97c30c11e4c149a59a270f687
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-04-01 11:17:03 +0300

    added output for input value

--- source/js/custom-range.js
+++ source/js/custom-range.js
@@ -7,6 +7,8 @@ var range = document.querySelector(".range");
 
 /*добавляем разметку для визуализации трека и ползунка*/
 range.insertAdjacentHTML('beforeend', '<div class="range__wrapper"><div class="range__wrap-inner"><div class="range__track"></div><div class="range__progress"></div></div><div class="range__handle"></div></div>');
+/*добавляем разметку отбражения значения value*/
+range.insertAdjacentHTML('beforeend', '<div class="range__value">');
 
 /*================*/
 var rangeInput = document.querySelector(".range__input");
@@ -14,8 +16,11 @@ var rangeInputMin = rangeInput['min'];
 var rangeInputMax = rangeInput['max'];
 var rangeMinMax = rangeInputMax - rangeInputMin; // это значение нужно, что бы корректно вычислять ширину progress и положение handle при input min отличном от 0.
 
+
 var rangeProgress = document.querySelector(".range__progress");
 var rangeHandle = document.querySelector(".range__handle");
+var output = document.querySelector('.range__value');
+output.innerHTML = rangeInput.value; //вывоим начальное значение value
 
 var widthRange = document.querySelector('.range__wrap-inner').offsetWidth; //узнать ширину range
 console.log('widthRange = ' + widthRange);
@@ -26,7 +31,7 @@ rangeInput.style.opacity = '0'; // задать стандартному input "
 rangeProgress.style.width = ((rangeInput.value - rangeInputMin) / rangeMinMax) * widthRange + 'px'; // установить начальную ширину progress/
 console.log(((rangeInput.value - rangeInputMin) / rangeMinMax) * widthRange + 'px');
 
-rangeHandle.style.left = (widthRange - (((rangeInput.value - rangeInputMin) / rangeMinMax) * widthRange)    - (widthHandle / 2)) + 'px'; // установить начальную точку handle
+rangeHandle.style.left = (widthRange - (((rangeInput.value - rangeInputMin) / rangeMinMax) * widthRange) - (widthHandle / 2)) + 'px'; // установить начальную точку handle
 console.log((((rangeInput.value - rangeInputMin) / rangeMinMax) * widthRange - (widthHandle / 2)) + 'px');
 
 // Изменить ширину .range__progress и координату handle (каждый раз когда двигаетм ползунок)  oninput/onchange
@@ -35,15 +40,14 @@ var isIE = /MSIE|Trident/.test(ua);
 
 if (isIE) {
   rangeInput.onchange = function () {
-    /*output.innerHTML = rangeInput.value;*/
+    output.innerHTML = rangeInput.value;
     rangeProgress.style.width = ((1 - ((rangeInput.value - rangeInputMin) / rangeMinMax)) * widthRange) + 'px';
     rangeHandle.style.left = (((rangeInput.value - rangeInputMin) / rangeMinMax) * widthRange - (widthHandle / 2)) + 'px';
   }
 
 } else {
   rangeInput.oninput = function () {
-    /*output.innerHTML = this.value;*/
-
+    output.innerHTML = this.value;
     rangeProgress.style.width = (1 - ((this.value - rangeInputMin) / rangeMinMax)) * widthRange + 'px';
     rangeHandle.style.left = (((this.value - rangeInputMin) / rangeMinMax) * widthRange - (widthHandle / 2)) + 'px';
   }
