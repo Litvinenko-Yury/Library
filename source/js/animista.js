cae27977240663c3a9d25c2d66b3fa38bbdd2404
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-06-09 19:53:09 +0300

    some minor edits animista.js

--- source/js/animista.js
+++ source/js/animista.js
@@ -1,15 +1,16 @@
 /*скрипт для добавления анимаций различных к элементам*/
 
 'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const btnAnim = document.querySelector(".animista");
 
-let btnAnim = document.querySelector(".animista");
+  btnAnim.addEventListener("mouseover", function (event) {
+    event.preventDefault();
+    btnAnim.classList.add("animista--rotate-in-center");
+  });
 
-btnAnim.addEventListener("mouseover", function (event) {
-  event.preventDefault();
-  btnAnim.classList.add("animista--rotate-in-center");
-});
-
-btnAnim.addEventListener("mouseout", function (event) {
-  event.preventDefault();
-  btnAnim.classList.remove("animista--rotate-in-center");
+  btnAnim.addEventListener("mouseout", function (event) {
+    event.preventDefault();
+    btnAnim.classList.remove("animista--rotate-in-center");
+  });
 });
