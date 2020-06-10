a194dbbef4bd155a855ad9df5366cfd523e99772
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-06-10 19:51:17 +0300

    modal.js - changed the function names

--- source/js/modal.js
+++ source/js/modal.js
@@ -9,14 +9,14 @@ const modalTrigger = document.querySelectorAll('[data-modal-open]'),
   modalContetnt = document.querySelector('.modal__content'),
   modalCloseBtn = document.querySelector('[data-modal-close]');
 
-function modalOpen() {
+function openModal() {
   event.preventDefault();
   modalOverlay.classList.add("modal__overlay--show");
   modalContetnt.classList.add("modal__content--show");
   document.body.style.overflow = 'hidden';
 }
 
-function modalClose() {
+function closeModal() {
   event.preventDefault();
   modalOverlay.classList.remove("modal__overlay--show");
   modalContetnt.classList.remove("modal__content--show");
@@ -25,22 +25,22 @@ function modalClose() {
 
 //открыть модал по кнопке с data-modal-open. Передаем функцию.
 modalTrigger.forEach((item) => {
-  item.addEventListener('click', modalOpen);
+  item.addEventListener('click', openModal);
 });
 
 //закрыть модал, способ-1: по кнопке с data-modal-close. Передаю функцию.
-modalCloseBtn.addEventListener('click', modalClose);
+modalCloseBtn.addEventListener('click', closeModal);
 
 //закрыть модал, способ-2:  по клику на темный фон
 modalOverlay.addEventListener('click', (event) => {
   if (event.target === modalOverlay) {
-    modalClose();
+    closeModal();
   }
 });
 
 //закрыть, способ-3:  модал по ESC
 window.addEventListener('keydown', (event) => {
   if (event.code === 'Escape' && modalOverlay.classList.contains('modal__overlay--show')) {
-    modalClose();
+    closeModal();
   }
 });
