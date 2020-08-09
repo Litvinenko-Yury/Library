6e81595ffc1e5f4ac9383adfd92c0dc01fc76c3b
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-08-09 20:16:39 +0300

    changed variable names in slider.js

--- source/js/slider.js
+++ source/js/slider.js
@@ -5,57 +5,57 @@
 
 'use strict';
 
-const slides = document.querySelectorAll('.slider__list-item'),
-  sliderBtnLeft = document.querySelector('#slider-prev'),
-  sliderBtnRight = document.querySelector('#slider-next'),
-  countCurrent = document.querySelector('#slider-current'),
-  countTotal = document.querySelector('#slider-total');
+const slides1 = document.querySelectorAll('.slider__list-item'),
+  sliderBtnLeft1 = document.querySelector('#slider-prev'),
+  sliderBtnRight1 = document.querySelector('#slider-next'),
+  countCurrent1 = document.querySelector('#slider-current'),
+  countTotal1 = document.querySelector('#slider-total');
 
-let sliderIndex = 0;
+let sliderIndex1 = 0;
 
 function showSlides(index, mass) {
   /*рассчет index на первом и последнем слайде */
   if (index < 0) {
-    sliderIndex = (mass.length - 1);
+    sliderIndex1 = (mass.length - 1);
   }
   if (index > (mass.length - 1)) {
-    sliderIndex = 0;
+    sliderIndex1 = 0;
   }
 
   mass.forEach((item) => { // скрыть все слайды
     item.classList.remove('slider__list-item--active');
   });
 
-  mass[sliderIndex].classList.add('slider__list-item--active');  // показать нужный слайд
-  setCountCurrent(sliderIndex);
+  mass[sliderIndex1].classList.add('slider__list-item--active');  // показать нужный слайд
+  setCountCurrent(sliderIndex1);
 }
 
 function setCountCurrent(index) {
   if (index < 10) {
-    countCurrent.textContent = `0${index + 1}`;
+    countCurrent1.textContent = `0${index + 1}`;
   } else {
-    countCurrent.textContent = `${index + 1}`;
+    countCurrent1.textContent = `${index + 1}`;
   }
 }
 
 function setCountTotal(mass) {
   if (mass.length < 9) {
-    countTotal.textContent = `0${mass.length}`;
+    countTotal1.textContent = `0${mass.length}`;
   } else {
-    countTotal.textContent = `${mass.length}`;
+    countTotal1.textContent = `${mass.length}`;
   }
 }
 
-showSlides(sliderIndex, slides);
-setCountTotal(slides);
-setCountCurrent(sliderIndex);
+showSlides(sliderIndex1, slides1);
+setCountTotal(slides1);
+setCountCurrent(sliderIndex1);
 
-sliderBtnLeft.addEventListener('click', () => {
-  sliderIndex--;
-  showSlides(sliderIndex, slides);
+sliderBtnLeft1.addEventListener('click', () => {
+  sliderIndex1--;
+  showSlides(sliderIndex1, slides1);
 });
 
-sliderBtnRight.addEventListener('click', () => {
-  sliderIndex++;
-  showSlides(sliderIndex, slides);
+sliderBtnRight1.addEventListener('click', () => {
+  sliderIndex1++;
+  showSlides(sliderIndex1, slides1);
 });
