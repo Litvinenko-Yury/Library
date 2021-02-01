682438c01fd2db0b206fd2436b99a5032b5b5539
Литвиненко Юрий <litvinenko-don@ukr.net>
2021-02-01 21:26:48 +0300

    Updates

--- js/bundle1.js
+++ js/bundle1.js
@@ -95,12 +95,13 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _modules_burger_some__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/burger-some */ "./source/js/modules/burger-some.js");
-/* harmony import */ var _modules_form_validation__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/form-validation */ "./source/js/modules/form-validation.js");
-/* harmony import */ var _modules_animista__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/animista */ "./source/js/modules/animista.js");
-/* harmony import */ var _modules_active_pseudo_to_input__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/active-pseudo-to-input */ "./source/js/modules/active-pseudo-to-input.js");
-/* harmony import */ var _modules_to_top__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules/to-top */ "./source/js/modules/to-top.js");
-/* harmony import */ var _modules_scroll_anchor__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules/scroll-anchor */ "./source/js/modules/scroll-anchor.js");
+/* harmony import */ var _modules_active_pseudo_to_input__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/active-pseudo-to-input */ "./source/js/modules/active-pseudo-to-input.js");
+/* harmony import */ var _modules_animista__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/animista */ "./source/js/modules/animista.js");
+/* harmony import */ var _modules_burger_some__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/burger-some */ "./source/js/modules/burger-some.js");
+/* harmony import */ var _modules_form_validation__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/form-validation */ "./source/js/modules/form-validation.js");
+/* harmony import */ var _modules_scroll_anchor__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules/scroll-anchor */ "./source/js/modules/scroll-anchor.js");
+/* harmony import */ var _modules_select_radio__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules/select-radio */ "./source/js/modules/select-radio.js");
+/* harmony import */ var _modules_to_top__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./modules/to-top */ "./source/js/modules/to-top.js");
 /**Entry points for scripts for page1.html */
 
 
@@ -112,12 +113,13 @@ __webpack_require__.r(__webpack_exports__);
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
-  Object(_modules_burger_some__WEBPACK_IMPORTED_MODULE_0__["default"])();
-  Object(_modules_form_validation__WEBPACK_IMPORTED_MODULE_1__["default"])();
-  Object(_modules_animista__WEBPACK_IMPORTED_MODULE_2__["default"])();
-  Object(_modules_active_pseudo_to_input__WEBPACK_IMPORTED_MODULE_3__["default"])();
-  Object(_modules_to_top__WEBPACK_IMPORTED_MODULE_4__["default"])();
-  Object(_modules_scroll_anchor__WEBPACK_IMPORTED_MODULE_5__["default"])();
+  Object(_modules_active_pseudo_to_input__WEBPACK_IMPORTED_MODULE_0__["default"])();
+  Object(_modules_animista__WEBPACK_IMPORTED_MODULE_1__["default"])();
+  Object(_modules_burger_some__WEBPACK_IMPORTED_MODULE_2__["default"])();
+  Object(_modules_form_validation__WEBPACK_IMPORTED_MODULE_3__["default"])();
+  Object(_modules_scroll_anchor__WEBPACK_IMPORTED_MODULE_4__["default"])();
+  Object(_modules_select_radio__WEBPACK_IMPORTED_MODULE_5__["default"])();
+  Object(_modules_to_top__WEBPACK_IMPORTED_MODULE_6__["default"])();
 });
 
 
@@ -478,6 +480,118 @@ function scrollAnchor() {
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (scrollAnchor);
+
+
+/***/ }),
+
+/***/ "./source/js/modules/select-radio.js":
+/*!*******************************************!*\
+  !*** ./source/js/modules/select-radio.js ***!
+  \*******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/*select-radio.js*/
+/*created by Yuri Litvinenko  https://litvinenko-yury.pro/*/
+
+function selectRadio() {
+  const formRadio = document.querySelector('#select-radio');
+  const selectAll = formRadio.querySelectorAll('.field-select');
+
+  /*===ФУНКЦИИ===*/
+  /* функция, расставляет data-attr для <label> в селекте*/
+  function setDataAttr(select) {
+    // передаю сюда отдельный select
+    //в этом select найти все input
+    let inputs = select.querySelectorAll('.field-select__input'); // найти  коллекцию inputs в селекте
+
+    inputs.forEach((elem) => {
+      if (elem.checked) {
+        elem.parentElement.lastElementChild.setAttribute('data-state', 'checked'); // находит родителя elem (т.е <li>), далее надит последнего ребенка родителя (т.е <label>), и этому <label> li,добавляет data-attr
+      }
+      if (elem.disabled) {
+        elem.parentElement.lastElementChild.setAttribute('data-state', 'disabled'); // нахоит родителя elem (т.е <li>), далее надит последнего ребенка родителя (т.е <label>), и этому <label> lj,добавляет data-attr
+      }
+    });
+  }
+
+  /* функция для шапки, записать textContent в шапку*/
+  function setTextContent(select) {
+    // передаю сюда отдельный select
+    //в этом select найти все label
+    //пройтись циклом по label, и найти тот, который data-state = 'checked';
+    //далее записать text.content этого label в text.content шапки
+
+    let labels = select.querySelectorAll('.field-select__label'),
+      title = select.querySelector('.field-select__title');
+
+    for (let i = 0; i < labels.length; i++) {
+      if (labels[i].dataset.state == 'checked') {
+        title.textContent = labels[i].textContent;
+        break;
+      }
+    }
+  }
+
+  /* функция для шапки, показать шапку, т.к. шапки изначальнео скрыты*/
+  function showTitle(select) {
+    // передаю сюда отдельный select
+    let title = select.querySelector('.field-select__title');
+    title.classList.remove('field-select__title--hide');
+  }
+
+  /* функция для контента, показать-скрыть блок контента */
+  function contentShowHide(select) {
+    // передаю сюда отдельный select
+    select.querySelector('.field-select__content').classList.toggle('field-select__content--hide');
+  }
+
+  /* функция для конкретной шапки, перекючить стрелку вверx-вниз */
+  function titleToggle(target) {
+    target.classList.toggle('field-select__title--open'); // вращение псевдоэлемента
+  }
+  /*===END ФУНКЦИИ===*/
+
+  /*==================*/
+  /*==================*/
+  selectAll.forEach((item) => {
+    /*начальные действия*/
+    setDataAttr(item); // расставить data-attr
+    setTextContent(item); // textContent от label записать в шапку
+    showTitle(item); // шапку показать
+    contentShowHide(item); // контент скрыть
+
+    /*действия по клику на шапку*/
+    let title = item.querySelector('.field-select__title');
+    title.addEventListener('click', (event) => {
+      titleToggle(event.target); //шапка - перекючить стрелку вверx-вниз
+      contentShowHide(item); // контент скрыть / показать
+    });
+
+    /*действия по клику на option*/
+    let labels = item.querySelectorAll('.field-select__label');
+    labels.forEach((elem) => {
+      elem.addEventListener('click', (event) => {
+        if (event.target.dataset.state !== 'disabled') {
+          title.textContent = event.target.textContent; //записать в шапку textContent от label
+          contentShowHide(item); // контент скрыть / показать
+        }
+      });
+    });
+  });
+
+  /*Reset шапки*/
+  const reset = document.querySelector('.reset');
+  reset.addEventListener('click', () => {
+    selectAll.forEach((item) => {
+      setTextContent(item); //  записать textContent в шапку
+    });
+  });
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (selectRadio);
 
 
 /***/ }),
--- js/bundle2.js
+++ js/bundle2.js
@@ -101,21 +101,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules2_main_nav__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules2/main-nav */ "./source/js/modules2/main-nav.js");
 /* harmony import */ var _modules2_modal_2__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules2/modal-2 */ "./source/js/modules2/modal-2.js");
 /* harmony import */ var _modules2_modal__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules2/modal */ "./source/js/modules2/modal.js");
-/* harmony import */ var _modules2_select_radio__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./modules2/select-radio */ "./source/js/modules2/select-radio.js");
-/* harmony import */ var _modules2_slider_carousel__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./modules2/slider-carousel */ "./source/js/modules2/slider-carousel.js");
-/* harmony import */ var _modules2_slider__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./modules2/slider */ "./source/js/modules2/slider.js");
-/* harmony import */ var _modules2_tabs__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./modules2/tabs */ "./source/js/modules2/tabs.js");
-/* harmony import */ var _modules2_timer_reverse__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./modules2/timer-reverse */ "./source/js/modules2/timer-reverse.js");
-/* harmony import */ var _modules2_typewriter__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./modules2/typewriter */ "./source/js/modules2/typewriter.js");
-/* harmony import */ var _modules_burger_some__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ./modules/burger-some */ "./source/js/modules/burger-some.js");
-/* harmony import */ var _modules_scroll_anchor__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ./modules/scroll-anchor */ "./source/js/modules/scroll-anchor.js");
-/* harmony import */ var _modules_to_top__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! ./modules/to-top */ "./source/js/modules/to-top.js");
+/* harmony import */ var _modules2_slider_carousel__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./modules2/slider-carousel */ "./source/js/modules2/slider-carousel.js");
+/* harmony import */ var _modules2_slider__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./modules2/slider */ "./source/js/modules2/slider.js");
+/* harmony import */ var _modules2_tabs__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./modules2/tabs */ "./source/js/modules2/tabs.js");
+/* harmony import */ var _modules2_timer_reverse__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./modules2/timer-reverse */ "./source/js/modules2/timer-reverse.js");
+/* harmony import */ var _modules2_typewriter__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./modules2/typewriter */ "./source/js/modules2/typewriter.js");
 /**Entry points for scripts for page2.html */
-
-
-
-
-
 
 
 
@@ -131,215 +122,18 @@ __webpack_require__.r(__webpack_exports__);
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
-  Object(_modules2_custom_range__WEBPACK_IMPORTED_MODULE_1__["default"])();
-  Object(_modules2_modal__WEBPACK_IMPORTED_MODULE_5__["default"])();
-  Object(_modules2_modal_2__WEBPACK_IMPORTED_MODULE_4__["default"])();
-  Object(_modules2_tabs__WEBPACK_IMPORTED_MODULE_9__["default"])();
   Object(_modules2_burger__WEBPACK_IMPORTED_MODULE_0__["default"])();
-  Object(_modules_burger_some__WEBPACK_IMPORTED_MODULE_12__["default"])();
-  Object(_modules2_main_nav__WEBPACK_IMPORTED_MODULE_3__["default"])();
-  Object(_modules_scroll_anchor__WEBPACK_IMPORTED_MODULE_13__["default"])();
-  Object(_modules_to_top__WEBPACK_IMPORTED_MODULE_14__["default"])();
-
-  Object(_modules2_timer_reverse__WEBPACK_IMPORTED_MODULE_10__["default"])();
-  Object(_modules2_typewriter__WEBPACK_IMPORTED_MODULE_11__["default"])();
-  Object(_modules2_select_radio__WEBPACK_IMPORTED_MODULE_6__["default"])();
-  Object(_modules2_slider__WEBPACK_IMPORTED_MODULE_8__["default"])();
-  Object(_modules2_slider_carousel__WEBPACK_IMPORTED_MODULE_7__["default"])();
   Object(_modules2_img_compare__WEBPACK_IMPORTED_MODULE_2__["default"])(); // запускающая функция для скрипта сравнения изображения, должна быть ниже всех
+  Object(_modules2_custom_range__WEBPACK_IMPORTED_MODULE_1__["default"])();
+  Object(_modules2_main_nav__WEBPACK_IMPORTED_MODULE_3__["default"])();
+  Object(_modules2_modal_2__WEBPACK_IMPORTED_MODULE_4__["default"])();
+  Object(_modules2_modal__WEBPACK_IMPORTED_MODULE_5__["default"])();
+  Object(_modules2_slider_carousel__WEBPACK_IMPORTED_MODULE_6__["default"])();
+  Object(_modules2_slider__WEBPACK_IMPORTED_MODULE_7__["default"])();
+  Object(_modules2_tabs__WEBPACK_IMPORTED_MODULE_8__["default"])();
+  Object(_modules2_timer_reverse__WEBPACK_IMPORTED_MODULE_9__["default"])();
+  Object(_modules2_typewriter__WEBPACK_IMPORTED_MODULE_10__["default"])();
 });
-
-
-/***/ }),
-
-/***/ "./source/js/modules/burger-some.js":
-/*!******************************************!*\
-  !*** ./source/js/modules/burger-some.js ***!
-  \******************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/*script  burger-some.js*/
-/*author  https://github.com/Litvinenko-Yury*/
-
-/*
-1. определяет, на каком бургере был клик;
-2. анимирует значек бургера - в "крестик";
-3.переключает класс элементу с нужным ID
-    */
-
-function burgerSome() {
-  document.addEventListener('click', function (event) { /*1. вешаем единый обработчик на элемент document*/
-    /*это нужно для того, что-бы много burger-ов можно было ставить в любом месте*/
-    let target = event.target; /*2. где был клик?*/
-    //console.log(target);
-
-    while (target != document) {
-      if (target.classList.contains("burger" || false)) { /* 3. на .burger? то, что надо!*/
-        //console.log("клик на бургер");
-        target.classList.toggle("burger--close");
-
-        let burgerDataID = target.getAttribute("data-target-id"); /*7. из переменной target читаем значение атрибута data-target-id*/
-        //console.log("нашел data-target-id = " + burgerDataID);
-
-        let burgerDataClassToggle = target.getAttribute("data-target-class-toggle"); /*8. из переменной target читаем значение атрибута data-target-class-toggle*/
-        //console.log("нашел data-target-class-toggle = " + burgerDataClassToggle);
-
-        let elementClassToggle = document.getElementById(burgerDataID); /* 9. находим элемент с нужным ID*/
-        //console.log("нашел элемент с нужным ID = " + elementClassToggle);
-
-        elementClassToggle.classList.toggle(burgerDataClassToggle);/* 10. Элементу с нужным ID меняем класс на переданный в data-target-class-toggle*/
-
-        break;
-      } else {/* 4. не на .burger? нет, не подходит... */
-        //console.log("промазал");
-        target = target.parentNode; /*5. поднимаем target на уровень вверх по иерархии родителей от event.target и выше*/
-        //console.log("вывожу target на уровень выше");
-        //console.log(target);
-      }
-    }
-    //console.log("вышел из цикла");
-  });
-
-  /* 6. Если клик был мимо burger, цикл дойдет вверх до document, и обработчик закончит работу*/
-}
-
-/* harmony default export */ __webpack_exports__["default"] = (burgerSome);
-
-
-/***/ }),
-
-/***/ "./source/js/modules/scroll-anchor.js":
-/*!********************************************!*\
-  !*** ./source/js/modules/scroll-anchor.js ***!
-  \********************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/*author: https://github.com/YuraVolk*/
-
-/*Скрипт плавный скрол к якорю*/
-
-function scrollAnchor() {
-  let isScrolling = false;
-
-  if (!String.prototype.startsWith) {
-    String.prototype.startsWith = function (searchString, position) {
-      position = position || 0;
-      return this.indexOf(searchString, position) === position;
-    };
-  }
-
-  function easeInOutQuad(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t + b;
-    t--;
-    return -c / 2 * (t * (t - 2) - 1) + b;
-  }
-
-  function smoothScrollTo(element, to, duration) {
-    isScrolling = true;
-    let start = element.scrollTop,
-      change = to - start,
-      currentTime = 0,
-      increment = 5;
-
-    let animateScroll = function animateScroll() {
-      currentTime += increment;
-      let val = easeInOutQuad(currentTime, start, change, duration);
-      window.pageYOffset = val;
-      // document.documentElement.scrollTop = val;
-      window.scroll(0, val);
-      if (currentTime < duration) {
-        setTimeout(animateScroll, increment);
-      }
-      if (currentTime === duration) {
-        isScrolling = false;
-      }
-    };
-
-    animateScroll();
-  }
-
-  function scrollAnimate(el) {
-    smoothScrollTo(document.documentElement, el.offsetTop, 400);
-  }
-
-  let links = document.getElementsByTagName('a');
-
-  for (let i = 0; i < links.length; i++) {
-    if (links[i].hasAttribute("href")) {
-      if (links[i].getAttribute("href").startsWith("#")) {
-
-        links[i].addEventListener("click", function (e) {
-
-          let elem = event.target;
-          e.preventDefault();
-
-          let el = document.getElementById(elem.getAttribute("href").substring(1));
-
-          if (el != null) {
-            scrollAnimate(el);
-          }
-        });
-      }
-    }
-  }
-}
-
-/* harmony default export */ __webpack_exports__["default"] = (scrollAnchor);
-
-
-/***/ }),
-
-/***/ "./source/js/modules/to-top.js":
-/*!*************************************!*\
-  !*** ./source/js/modules/to-top.js ***!
-  \*************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/*script  to-top.js*/
-/*author  https://github.com/Litvinenko-Yury*/
-
-/*Ссылка «вверх» в нижнем правом углу страницы.*/
-
-function toTop() {
-  const toTop = document.querySelector(".to-top");
-
-  /*При старте скрипта добавить класс .to-top--visible*/
-  toTop.classList.add("to-top--no-visible");
-
-  /*объявление функции с добавлением класса .to-top--visible*/
-  function addСlassNoVisible() {
-    toTop.classList.add("to-top--no-visible");
-  }
-
-  /*объявление функции с удалением класса .to-top--visible*/
-  function delСlassNoVisible() {
-    toTop.classList.remove("to-top--no-visible");
-  }
-
-  window.addEventListener("scroll", function () {
-    /*в переменную posYVisible записываем число пикселей, на которое документ пролистали в данный момент по вертикали*/
-    let posYVisible = window.scrollY;
-
-    if (posYVisible > 500) {
-      delСlassNoVisible();
-    }
-    else {
-      addСlassNoVisible();
-    }
-  });
-}
-
-/* harmony default export */ __webpack_exports__["default"] = (toTop);
 
 
 /***/ }),
@@ -766,118 +560,6 @@ function modal() {
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (modal);
-
-
-/***/ }),
-
-/***/ "./source/js/modules2/select-radio.js":
-/*!********************************************!*\
-  !*** ./source/js/modules2/select-radio.js ***!
-  \********************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/*select-radio.js*/
-/*created by Yuri Litvinenko  https://litvinenko-yury.pro/*/
-
-function selectRadio() {
-  const formRadio = document.querySelector('#select-radio');
-  const selectAll = formRadio.querySelectorAll('.field-select');
-
-  /*===ФУНКЦИИ===*/
-  /* функция, расставляет data-attr для <label> в селекте*/
-  function setDataAttr(select) {
-    // передаю сюда отдельный select
-    //в этом select найти все input
-    let inputs = select.querySelectorAll('.field-select__input'); // найти  коллекцию inputs в селекте
-
-    inputs.forEach((elem) => {
-      if (elem.checked) {
-        elem.parentElement.lastElementChild.setAttribute('data-state', 'checked'); // находит родителя elem (т.е <li>), далее надит последнего ребенка родителя (т.е <label>), и этому <label> li,добавляет data-attr
-      }
-      if (elem.disabled) {
-        elem.parentElement.lastElementChild.setAttribute('data-state', 'disabled'); // нахоит родителя elem (т.е <li>), далее надит последнего ребенка родителя (т.е <label>), и этому <label> lj,добавляет data-attr
-      }
-    });
-  }
-
-  /* функция для шапки, записать textContent в шапку*/
-  function setTextContent(select) {
-    // передаю сюда отдельный select
-    //в этом select найти все label
-    //пройтись циклом по label, и найти тот, который data-state = 'checked';
-    //далее записать text.content этого label в text.content шапки
-
-    let labels = select.querySelectorAll('.field-select__label'),
-      title = select.querySelector('.field-select__title');
-
-    for (let i = 0; i < labels.length; i++) {
-      if (labels[i].dataset.state == 'checked') {
-        title.textContent = labels[i].textContent;
-        break;
-      }
-    }
-  }
-
-  /* функция для шапки, показать шапку, т.к. шапки изначальнео скрыты*/
-  function showTitle(select) {
-    // передаю сюда отдельный select
-    let title = select.querySelector('.field-select__title');
-    title.classList.remove('field-select__title--hide');
-  }
-
-  /* функция для контента, показать-скрыть блок контента */
-  function contentShowHide(select) {
-    // передаю сюда отдельный select
-    select.querySelector('.field-select__content').classList.toggle('field-select__content--hide');
-  }
-
-  /* функция для конкретной шапки, перекючить стрелку вверx-вниз */
-  function titleToggle(target) {
-    target.classList.toggle('field-select__title--open'); // вращение псевдоэлемента
-  }
-  /*===END ФУНКЦИИ===*/
-
-  /*==================*/
-  /*==================*/
-  selectAll.forEach((item) => {
-    /*начальные действия*/
-    setDataAttr(item); // расставить data-attr
-    setTextContent(item); // textContent от label записать в шапку
-    showTitle(item); // шапку показать
-    contentShowHide(item); // контент скрыть
-
-    /*действия по клику на шапку*/
-    let title = item.querySelector('.field-select__title');
-    title.addEventListener('click', (event) => {
-      titleToggle(event.target); //шапка - перекючить стрелку вверx-вниз
-      contentShowHide(item); // контент скрыть / показать
-    });
-
-    /*действия по клику на option*/
-    let labels = item.querySelectorAll('.field-select__label');
-    labels.forEach((elem) => {
-      elem.addEventListener('click', (event) => {
-        if (event.target.dataset.state !== 'disabled') {
-          title.textContent = event.target.textContent; //записать в шапку textContent от label
-          contentShowHide(item); // контент скрыть / показать
-        }
-      });
-    });
-  });
-
-  /*Reset шапки*/
-  const reset = document.querySelector('.reset');
-  reset.addEventListener('click', () => {
-    selectAll.forEach((item) => {
-      setTextContent(item); //  записать textContent в шапку
-    });
-  });
-}
-
-/* harmony default export */ __webpack_exports__["default"] = (selectRadio);
 
 
 /***/ }),
