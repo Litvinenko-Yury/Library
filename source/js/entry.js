e8358dfc79bb24756ffc7168d004af28aa2e7ce3
Литвиненко Юрий <litvinenko-don@ukr.net>
2021-02-01 21:26:20 +0300

    feat: добавил custom select - одиночный и мультселект

--- source/js/entry.js
+++ source/js/entry.js
@@ -1,18 +1,19 @@
 /**Entry points for scripts for page1.html */
+import activePseudoToInput from './modules/active-pseudo-to-input';
+import animista from './modules/animista';
 import burgerSome from './modules/burger-some';
 import formValid from './modules/form-validation';
-import animista from './modules/animista';
-import activePseudoToInput from './modules/active-pseudo-to-input';
-import toTop from './modules/to-top';
 import scrollAnchor from './modules/scroll-anchor';
-
+import selectRadio from './modules/select-radio';
+import toTop from './modules/to-top';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
+  activePseudoToInput();
+  animista();
   burgerSome();
   formValid();
-  animista();
-  activePseudoToInput();
-  toTop();
   scrollAnchor();
+  selectRadio();
+  toTop();
 });
--- source/js/entry2.js
+++ source/js/entry2.js
@@ -5,34 +5,24 @@ import imgCompare from './modules2/img-compare';
 import mainNav from './modules2/main-nav';
 import modal2 from './modules2/modal-2';
 import modal from './modules2/modal';
-import selectRadio from './modules2/select-radio';
+
 import sliderCarousel from './modules2/slider-carousel';
 import slider from './modules2/slider';
 import tabs from './modules2/tabs';
 import timerReverse from './modules2/timer-reverse';
 import typewriter from './modules2/typewriter';
 
-
-import burgeSome from './modules/burger-some';
-import scrollAnchor from './modules/scroll-anchor';
-import toTop from './modules/to-top';
-
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
-  customRange();
-  modal();
-  modal2();
-  tabs();
   burger();
-  burgeSome();
+  imgCompare(); // запускающая функция для скрипта сравнения изображения, должна быть ниже всех
+  customRange();
   mainNav();
-  scrollAnchor();
-  toTop();
-
+  modal2();
+  modal();
+  sliderCarousel();
+  slider();
+  tabs();
   timerReverse();
   typewriter();
-  selectRadio();
-  slider();
-  sliderCarousel();
-  imgCompare(); // запускающая функция для скрипта сравнения изображения, должна быть ниже всех
 });
