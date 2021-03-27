8beb983c8e4861a45d8bdae7b5d3757629f49fca
Литвиненко Юрий <litvinenko-don@ukr.net>
2021-03-27 14:45:00 +0300

    Updates

--- js/check-browser.js
+++ js/check-browser.js
@@ -1,16 +1,14 @@
 /*check-browser.js*/
 /*created by Yuri Litvinenko  https://litvinenko-yury.pro/*/
 
-
-document.getElementById('checkBrowser').style.display = 'block';
-
-
 /*проверка на IE - (трюк: он не использует append)*/
-//var div = document.createElement('div');
 
-//try {
-//  !!document.body.append(div); // приводим результат к булевому типу (true или false) с помощью приёма двойного отрицания
-//} catch (error) {
-//  document.getElementById('checkBrowser').style.display = 'block'; // это модальное окно с сообщением - "Ваш браузер устарел"
-  //return
-//}
+var div = document.createElement('div');
+
+try {
+  !!document.body.append(div); // приводим результат к булевому типу (true или false) с помощью приёма двойного отрицания
+} catch (error) {
+  document.getElementById('checkBrowser').style.display = 'block'; // это модальное окно с сообщением - "Ваш браузер устарел"
+  return;
+}
+
