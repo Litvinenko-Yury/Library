24ed40c671ada863b3886f01f910d4e1c7895b6a
Литвиненко Юрий <litvinenko-don@ukr.net>
2021-03-27 14:44:42 +0300

    fix: bag in .check-browser.js

--- source/js/modules2/check-browser.js
+++ source/js/modules2/check-browser.js
@@ -2,10 +2,13 @@
 /*created by Yuri Litvinenko  https://litvinenko-yury.pro/*/
 
 /*проверка на IE - (трюк: он не использует append)*/
+
+var div = document.createElement('div');
+
 try {
   !!document.body.append(div); // приводим результат к булевому типу (true или false) с помощью приёма двойного отрицания
 } catch (error) {
   document.getElementById('checkBrowser').style.display = 'block'; // это модальное окно с сообщением - "Ваш браузер устарел"
-  //return
+  return;
 }
 
